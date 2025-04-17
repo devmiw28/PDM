ff52260614b064fa3349e2d85c0b7cab1f81aecd
MJM <MJM@LAPTOP-AJRH9J1H>
2025-04-18 00:55:32 +0800

    move

--- PDMAIRLINE/Airline/Help.Designer.cs
+++ PDMAIRLINE/Airline/Help.Designer.cs
@@ -32,9 +32,9 @@
             this.btnSearch = new System.Windows.Forms.Button();
             this.listBoxResults = new System.Windows.Forms.ListBox();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.pnlHeader = new System.Windows.Forms.Panel();
             this.label1 = new System.Windows.Forms.Label();
             this.panel2 = new System.Windows.Forms.Panel();
+            this.pnlHeader = new System.Windows.Forms.Panel();
             this.btnExit = new System.Windows.Forms.Button();
             this.panel1.SuspendLayout();
             this.pnlHeader.SuspendLayout();
@@ -45,18 +45,20 @@
             this.txtSearch.BackColor = System.Drawing.SystemColors.HighlightText;
             this.txtSearch.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.txtSearch.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtSearch.Location = new System.Drawing.Point(372, 313);
+            this.txtSearch.Location = new System.Drawing.Point(496, 385);
+            this.txtSearch.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtSearch.Name = "txtSearch";
-            this.txtSearch.Size = new System.Drawing.Size(336, 31);
+            this.txtSearch.Size = new System.Drawing.Size(447, 37);
             this.txtSearch.TabIndex = 0;
             this.txtSearch.TextChanged += new System.EventHandler(this.txtSearch_TextChanged);
             // 
             // btnSearch
             // 
             this.btnSearch.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnSearch.Location = new System.Drawing.Point(710, 313);
+            this.btnSearch.Location = new System.Drawing.Point(1003, 383);
+            this.btnSearch.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnSearch.Name = "btnSearch";
-            this.btnSearch.Size = new System.Drawing.Size(49, 32);
+            this.btnSearch.Size = new System.Drawing.Size(65, 39);
             this.btnSearch.TabIndex = 1;
             this.btnSearch.Text = "🔎";
             this.btnSearch.UseVisualStyleBackColor = true;
@@ -67,10 +69,11 @@
             this.listBoxResults.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.listBoxResults.Font = new System.Drawing.Font("Arial Narrow", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.listBoxResults.FormattingEnabled = true;
-            this.listBoxResults.ItemHeight = 17;
-            this.listBoxResults.Location = new System.Drawing.Point(372, 351);
+            this.listBoxResults.ItemHeight = 22;
+            this.listBoxResults.Location = new System.Drawing.Point(496, 432);
+            this.listBoxResults.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.listBoxResults.Name = "listBoxResults";
-            this.listBoxResults.Size = new System.Drawing.Size(399, 102);
+            this.listBoxResults.Size = new System.Drawing.Size(532, 110);
             this.listBoxResults.TabIndex = 5;
             // 
             // panel1
@@ -81,10 +84,34 @@
             this.panel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.panel1.Controls.Add(this.label1);
             this.panel1.Controls.Add(this.panel2);
-            this.panel1.Location = new System.Drawing.Point(0, 70);
+            this.panel1.Location = new System.Drawing.Point(0, 86);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(1300, 220);
+            this.panel1.Size = new System.Drawing.Size(1733, 271);
             this.panel1.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Britannic Bold", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Gold;
+            this.label1.Location = new System.Drawing.Point(216, 65);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(627, 56);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "PDM AIRLINE HELP CENTER";
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.Transparent;
+            this.panel2.BackgroundImage = global::Airline.Properties.Resources.PDM_AIRLINE_HELP_CENTER_LOGO_removebg_preview__3__removebg_preview_removebg_preview__1_;
+            this.panel2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.panel2.Location = new System.Drawing.Point(15, 33);
+            this.panel2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(196, 182);
+            this.panel2.TabIndex = 0;
             // 
             // pnlHeader
             // 
@@ -94,34 +121,11 @@
             this.pnlHeader.Controls.Add(this.btnExit);
             this.pnlHeader.Dock = System.Windows.Forms.DockStyle.Top;
             this.pnlHeader.Location = new System.Drawing.Point(0, 0);
+            this.pnlHeader.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.pnlHeader.Name = "pnlHeader";
-            this.pnlHeader.Size = new System.Drawing.Size(1300, 70);
+            this.pnlHeader.Size = new System.Drawing.Size(1733, 86);
             this.pnlHeader.TabIndex = 16;
             this.pnlHeader.Paint += new System.Windows.Forms.PaintEventHandler(this.pnlHeader_Paint);
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.BackColor = System.Drawing.Color.Transparent;
-            this.label1.Font = new System.Drawing.Font("Britannic Bold", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.ForeColor = System.Drawing.Color.Gold;
-            this.label1.Location = new System.Drawing.Point(162, 53);
-            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(498, 44);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "PDM AIRLINE HELP CENTER";
-            // 
-            // panel2
-            // 
-            this.panel2.BackColor = System.Drawing.Color.Transparent;
-            this.panel2.BackgroundImage = global::Airline.Properties.Resources.PDM_AIRLINE_HELP_CENTER_LOGO_removebg_preview__3__removebg_preview_removebg_preview__1_;
-            this.panel2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.panel2.Location = new System.Drawing.Point(11, 27);
-            this.panel2.Margin = new System.Windows.Forms.Padding(2);
-            this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(147, 148);
-            this.panel2.TabIndex = 0;
             // 
             // btnExit
             // 
@@ -132,9 +136,10 @@
             this.btnExit.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.btnExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnExit.ForeColor = System.Drawing.Color.Gold;
-            this.btnExit.Location = new System.Drawing.Point(1248, 12);
+            this.btnExit.Location = new System.Drawing.Point(1664, 15);
+            this.btnExit.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnExit.Name = "btnExit";
-            this.btnExit.Size = new System.Drawing.Size(40, 40);
+            this.btnExit.Size = new System.Drawing.Size(53, 49);
             this.btnExit.TabIndex = 21;
             this.btnExit.Text = "x";
             this.btnExit.UseVisualStyleBackColor = false;
@@ -142,17 +147,18 @@
             // 
             // Help
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Snow;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ClientSize = new System.Drawing.Size(1300, 750);
+            this.ClientSize = new System.Drawing.Size(1733, 923);
             this.Controls.Add(this.pnlHeader);
             this.Controls.Add(this.listBoxResults);
             this.Controls.Add(this.btnSearch);
             this.Controls.Add(this.txtSearch);
             this.Controls.Add(this.panel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Help";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form5";
